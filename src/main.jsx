7e6d1d4b6debf2d54f50b97d9c481d31dcf254e0
inwoo5267 <gjy5267@gmail.com>
2026-04-21 18:04:54 +0900

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx' // 위에서 작성한 코드가 들어있는 파일
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
